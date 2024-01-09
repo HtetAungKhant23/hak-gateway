54b9382a3f1b99e2940814c6498cec775feb5056
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-09 15:45:28 +0630

    add: set and get controller for redis test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiBody, ApiProperty } from '@nestjs/swagger';
+
+export class JsonType {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  phone: string;
+}
 
 @Controller('test')
 export class AppController {
@@ -8,5 +17,16 @@ export class AppController {
   @Get()
   getHello() {
     return this.appService.getHello();
+  }
+
+  @Post('redis')
+  @ApiBody({ description: 'asdf', type: JsonType })
+  setJSONtoRedis(@Body() data: JsonType) {
+    return this.appService.setJSON(data);
+  }
+
+  @Get('redis')
+  getJSONfromRedis() {
+    return this.appService.getJSON();
   }
 }
