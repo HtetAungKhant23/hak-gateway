121d2faba82a1a737bb447437aa8929a0a44ae6d
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-19 12:00:35 +0630

    add: passportModule and JwtModule in auth module and includ its notes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
-    // PassportModule.register({ defaultStrategy: 'jwt' }),
-    // JwtModule,
+    // ? ensure that the verify phase performed by Passport, and the sign phase performed in our AuthService, use a common secret.
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
     ClientsModule.register([
       {
         name: 'AUTH_SERVICE',
--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,7 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     // ? verify jwt token in parent class cause of super method and then called validate method
     super({
