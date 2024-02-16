0133fca19048dd15b2fd6e19066b37c142d041c2
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-02-16 11:06:57 +0630

    learning error interceptor and redo customize

--- src/libs/interceptor/error.interceptor.ts
+++ src/libs/interceptor/error.interceptor.ts
@@ -5,6 +5,7 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import { error } from 'console';
 import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable()
@@ -13,80 +14,95 @@ export class ErrorInterceptor implements NestInterceptor {
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
+    console.log('this is error');
+
     return next.handle().pipe(
       catchError((err) => {
-        return throwError(() => doException(err));
+        // return throwError(() => doException(err));
+        const error = {
+          meta: {
+            success:
+              err.statusCode >= 200 && err.statusCode <= 300 ? true : false,
+            message: err.message,
+            devMessage: err.devMessage,
+          },
+          body: null,
+        };
+        return throwError(
+          () => new HttpException(error, err.status || err.statusCode),
+        );
       }),
     );
   }
 }
 
-const doException = (err) => {
-  try {
-    let error = {};
-    if (err.response?.status === 'error') {
-      error = {
-        meta: {
-          success: err.status >= 200 && err.status <= 300 ? true : false,
-          messageEn: err.response.messageEn,
-          messageMm: err.response.messageMm,
-        },
-        body: null,
-      };
-      return new HttpException(error, err.status || err.statusCode);
-    } else {
-      if (err.error) {
-        error = {
-          meta: {
-            success: err?.status >= 200 && err?.status <= 300 ? true : false,
-            messageEn: err.messageEn,
-            messageMm: err.messageMm,
-          },
-          body: null,
-        };
-      }
-      if (err.response && err.response.error) {
-        error = {
-          meta: {
-            success:
-              err.response.statusCode >= 200 && err.response.statusCode <= 300
-                ? true
-                : false,
-            messageEn: err.response.message[0],
-            messageMm: err.response.message[0],
-          },
-          body: null,
-        };
-      }
+// const doException = (err) => {
+//   try {
+//     let error = {};
+//     if (err.response?.status === 'error') {
+//       error = {
+//         meta: {
+//           success: err.status >= 200 && err.status <= 300 ? true : false,
+//           messageEn: err.response.messageEn,
+//           messageMm: err.response.messageMm,
+//         },
+//         body: null,
+//       };
+//       return new HttpException(error, err.status || err.statusCode);
+//     } else {
+//       if (err.error) {
+//         error = {
+//           meta: {
+//             success:
+//               err.statusCode >= 200 && err.statusCode <= 300 ? true : false,
+//             message: err.message,
+//             devMessage: err.devMessage,
+//           },
+//           body: null,
+//         };
+//       }
+//       if (err.response && err.response.error) {
+//         error = {
+//           meta: {
+//             success:
+//               err.response.statusCode >= 200 && err.response.statusCode <= 300
+//                 ? true
+//                 : false,
+//             messageEn: err.response.message[0],
+//             messageMm: err.response.message[0],
+//           },
+//           body: null,
+//         };
+//       }
 
-      if (err.response && err.response.messageMm && err.response.messageEn) {
-        error = {
-          meta: {
-            success:
-              err.response.statusCode ??
-              (err.response.status >= 200 && err.response.statusCode) ??
-              err.response.status <= 300
-                ? true
-                : false,
-            messageEn: err.response.messageMm,
-            messageMm: err.response.messageEn,
-          },
-          body: null,
-        };
-      }
+//       if (err.response && err.response.messageMm && err.response.messageEn) {
+//         error = {
+//           meta: {
+//             success:
+//               err.response.statusCode ??
+//               (err.response.status >= 200 && err.response.statusCode) ??
+//               err.response.status <= 300
+//                 ? true
+//                 : false,
+//             messageEn: err.response.messageMm,
+//             messageMm: err.response.messageEn,
+//           },
+//           body: null,
+//         };
+//       }
 
-      return new HttpException(error, err.status || err.statusCode);
-    }
-  } catch (e) {
-    console.log(e);
-    const error = {
-      meta: {
-        success: false,
-        messageEn: err?.messageEn || 'Internal Server Error',
-        messageMm: err?.messageMm || 'Internal Server Error',
-      },
-      body: null,
-    };
-    return new HttpException(error, err?.statusCode || 500);
-  }
-};
+//       return new HttpException(error, err.status || err.statusCode);
+//     }
+//   } catch (e) {
+//     console.log(e, 'ee');
+//     const error = {
+//       meta: {
+//         success: false,
+//         messageEn: err?.messageEn || 'Internal Server Error',
+//         messageMm: err?.messageMm || 'Internal Server Error',
+//       },
+//       body: null,
+//     };
+//     return new HttpException(error, err?.statusCode || 500);
+//   }
+// };
